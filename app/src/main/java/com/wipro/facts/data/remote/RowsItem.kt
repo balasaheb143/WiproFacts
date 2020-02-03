bbf3745b3c28e9e46c9ee578df7ce90b779927b6
Balasaheb Dhumal <balasahebvdhumal@gmail.com>
2020-02-03 11:09:22 +0530

    Update RowsItem.kt
    update data type var

--- app/src/main/java/com/wipro/facts/data/remote/RowsItem.kt
+++ app/src/main/java/com/wipro/facts/data/remote/RowsItem.kt
@@ -5,11 +5,11 @@ import com.google.gson.annotations.SerializedName
 data class RowsItem(
 
     @field:SerializedName("imageHref")
-    val imageHref: String? = null,
+    var imageHref: String? = null,
 
     @field:SerializedName("description")
-    val description: String? = null,
+    var description: String? = null,
 
     @field:SerializedName("title")
-    val title: String? = null
+    var title: String? = null
 )
